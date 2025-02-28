a2bed254f147f22bfc98aa8d162874cb97690a5d
Thiago Mota <euthiagojose2004@gmail.com>
2025-02-28 15:40:11 -0300

    first commit

---
+++ src/main/java/com/br/minispc/MinispcApplication.java
@@ -0,0 +1,13 @@
+package com.br.minispc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinispcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinispcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/minispc/MinispcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.minispc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinispcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
